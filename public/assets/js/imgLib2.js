da69c86557c4d6cf5e1ce33a8673ca169f0c6bab
Rohan Rogers <sarith21@gmail.com>
2022-05-07 16:23:43 -0700

    Fix indentation

--- public/assets/js/imgLib2.js
+++ public/assets/js/imgLib2.js
@@ -7,41 +7,41 @@ window.addEventListener("load", function() {
     let fileName = "";
     // upload File
     uploadFile.addEventListener("change", () => {
-    const ctx = canvas.getContext("2d");
-      const file = document.getElementById("upload-file-2").files[0];
-      // init FileReader API
-      const reader = new FileReader();
-      if (file) {
-        fileName = file.name;
-        // read data as URL
-        reader.readAsDataURL(file);
-      }
+        const ctx = canvas.getContext("2d");
+        const file = document.getElementById("upload-file-2").files[0];
+        // init FileReader API
+        const reader = new FileReader();
+        if (file) {
+            fileName = file.name;
+            // read data as URL
+            reader.readAsDataURL(file);
+        }
 
-      // add image to canvas
-      reader.addEventListener("load", () => {
-        img = new Image();
-        img.src = reader.result;
-        img.onload = function () {
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img, 0, 0, img.width, img.height);
-        };
-      }, false);
+        // add image to canvas
+        reader.addEventListener("load", () => {
+            img = new Image();
+            img.src = reader.result;
+            img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+            };
+        }, false);
     });
 
     const downloadBtn = document.getElementById("download-2");
     downloadBtn.addEventListener("click", () => {
-      const fileExtension = fileName.slice(-4);
-      let newFilename;
+        const fileExtension = fileName.slice(-4);
+        let newFilename;
 
-      if (fileExtension === ".jpg" || fileExtension === ".png") {
-        newFilename = fileName.substring(0, fileName.length - 4) + "-ukraine.jpg";
-      }
+        if (fileExtension === ".jpg" || fileExtension === ".png") {
+            newFilename = fileName.substring(0, fileName.length - 4) + "-ukraine.jpg";
+        }
 
-      const link = document.createElement("a");
-      link.download = newFilename;
-      link.href = canvas.toDataURL("image/jpeg", 0.8);
-      link.dispatchEvent(new MouseEvent("click"));
+        const link = document.createElement("a");
+        link.download = newFilename;
+        link.href = canvas.toDataURL("image/jpeg", 0.8);
+        link.dispatchEvent(new MouseEvent("click"));
     });
 
     const thresholdSlider = document.getElementById("threshold-2");
